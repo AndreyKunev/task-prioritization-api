5346b50588c9ed947820cb4a220f385a45b795ef
Andrey Kunev <akunev45@gmail.com>
2025-02-01 15:42:34 +0200

    Changed file structure

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const host = process.env.HOST;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(
+		`server running : http://${host}:${port}`
+	);
 });
