f138ffb034b669e43efd396bb97bf0146e0e998b
Andrey Kunev <akunev45@gmail.com>
2025-02-03 15:09:15 +0200

    add#2: added getTaskById and getTasksByPriority

--- src/controllers/tasks-controllers.js
+++ src/controllers/tasks-controllers.js
@@ -1,6 +1,4 @@
-import { randomUUID } from 'crypto';
-
-import { calculatePriority } from '../utils/taskUtils.js';
+import { calculatePriority, mergeSort } from '../utils/taskUtils.js';
 
 let DUMMY_TASKS = [
 	{
@@ -9,7 +7,8 @@ let DUMMY_TASKS = [
 		description:
 			'Resolve the issue preventing users from logging in with Google OAuth.',
 		priority: 'high',
-		dueDate: '2025-02-01',
+		dueDate: '2025-02-03',
+		isCritical: true,
 		isCompleted: false,
 	},
     {
@@ -17,16 +16,37 @@ let DUMMY_TASKS = [
 		title: 'Buy pants',
 		description:
 			'Buy new pair of pants.',
-		priority: 'high',
+		priority: 'medium',
 		dueDate: '2025-02-12',
+		isCritical: false,
 		isCompleted: false,
 	},
+	{
+		id: '17181921',
+		title: 'Book plane tickets',
+		description:
+			'Book plane tickets for April trip.',
+		priority: 'high',
+		dueDate: '2025-02-04',
+		isCritical: true,
+		isCompleted: false,
+	},
+	{
+		id: '100129',
+		title: 'Get new coffee machine',
+		description:
+			'You need to replace your old coffee machine.',
+		priority: 'low',
+		dueDate: '2025-02-26',
+		isCritical: false,
+		isCompleted: false,
+	}
 ];
 
 export const createTask = (req, res, next) => {
 	const { title, description, dueDate, isCritical } = req.body;
 
-	let priority = calculatePriority(isCritical, dueDate);
+	const priority = calculatePriority(isCritical, dueDate);
 
 	const createdTask = {
 		id: crypto.randomUUID(),
@@ -34,7 +54,6 @@ export const createTask = (req, res, next) => {
 		description,
 		priority,
 		dueDate,
-		isCritical,
 		isCompleted: false,
 	};
 
@@ -42,3 +61,30 @@ export const createTask = (req, res, next) => {
 
     res.status(201).json({task: createdTask});
 };
+
+export const getTaskById = (req, res, next) => {
+	const targetId = req.params.taskId;
+
+	const targetTask = DUMMY_TASKS.find((task) => {
+		return task.id === targetId;
+	})
+
+	if (!targetTask) {
+		const error = new Error('Could not find task with provided ID.')
+		error.code = 404;
+		return next(error);
+	}
+
+	res.json({ targetTask });
+}
+
+export const getTasksByPriority = (req, res, next) => {
+	const { sort } = req.query;
+
+	if (sort === 'priority') {
+		const sortedTasks = mergeSort(DUMMY_TASKS);
+		return res.json({ tasks: sortedTasks });
+	}
+
+	res.json({ tasks: DUMMY_TASKS});
+}
