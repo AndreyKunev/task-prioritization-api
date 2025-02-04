c75d9ecbb05bf659b6405a3634eb9f2f965d2dbe
Andrey Kunev <akunev45@gmail.com>
2025-02-04 15:31:52 +0200

    add#7: added getter that maps _id to id

--- src/models/task.js
+++ src/models/task.js
@@ -1,13 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-		title: {type: String, required: true},
-		description: String,
-		priority: {type: String, required: true},
-		dueDate: Date,
-		isCompleted: {type: Boolean, required: true}
-})
+	title: { type: String, required: true },
+	description: String,
+	priority: { type: String, required: true },
+	dueDate: Date,
+	isCompleted: { type: Boolean, required: true },
+});
+
+taskSchema.virtual('id').get(function () {
+	return this._id.toHexString();
+});
+
+taskSchema.set('toJSON', { virtuals: true });
 
 export const Task = mongoose.model('Task', taskSchema);
