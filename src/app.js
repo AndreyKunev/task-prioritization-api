2f00a679e5448d90425b05fc2c1ee49f3750293e
Andrey Kunev <akunev45@gmail.com>
2025-02-04 10:03:48 +0200

    add#6: mongoose setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 import dotenv from 'dotenv';
 
 import { routes } from './routes/index.js';
@@ -8,14 +10,24 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 const host = process.env.HOST;
-
+const mongoURI = process.env.MONGO_URI;
 
 app.use(express.json());
 
-app.use('/', routes)
+app.use('/', routes);
 
-app.listen(port, () => {
-	console.log(
-		`server running : http://${host}:${port}`
-	);
-});
+const startServer = async () => {
+	try {
+		await mongoose.connect(mongoURI);
+		console.log('Connected to MongoDB');
+
+		app.listen(port, () => {
+			console.log(`Server running: http://${host}:${port}`);
+		});
+	} catch (error) {
+		console.error('MongoDB connection error:', error);
+		process.exit(1);
+	}
+};
+
+startServer();
